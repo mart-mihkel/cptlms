e2e1968ed95390f15e062813666b1bba0bfac8f5
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-11-27 22:44:38 +0200

    no answer squad examples

--- cptlms/squad.py
+++ cptlms/squad.py
@@ -1,73 +1,130 @@
 import logging
-from typing import cast
+from typing import TypedDict, cast
 
-from datasets.load import load_dataset
 from datasets.dataset_dict import DatasetDict
-from evaluate.module import EvaluationModule
-from transformers import BatchEncoding, PreTrainedTokenizerFast
+from datasets.load import load_dataset
+from transformers import PreTrainedTokenizerFast
 
 logger = logging.getLogger(__name__)
 
 
-class SQuAD:
-    data: DatasetDict
-    metric: EvaluationModule
+class SQuADBatchAnswer(TypedDict):
+    answer_start: tuple[int]
+    text: tuple[str]
 
-    def __init__(self) -> None:
+
+class SQuADBatch(TypedDict):
+    id: list[int]
+    question: list[str]
+    context: list[str]
+    answers: list[SQuADBatchAnswer]
+
+
+class SQuAD:
+    _max_len = 384
+    _stride = 128
+
+    def __init__(self, tokenizer: PreTrainedTokenizerFast) -> None:
         logger.info("init SQuAD")
 
-        self.data = cast(DatasetDict, load_dataset("squad"))
+        data = load_dataset("squad")
+        assert isinstance(data, DatasetDict)
 
-    def tokenize(self, tokenizer: PreTrainedTokenizerFast):
+        self.data = data
+        self.tokenizer = tokenizer
+        self.tokenized = self._tokenize()
+
+    def _tokenize(self) -> DatasetDict:
         logger.info("tokenize SQuAD")
 
-        def _preprocess(examples: dict) -> BatchEncoding:
-            questions = [q.strip() for q in examples["question"]]
-            inputs = tokenizer(
-                questions,
-                examples["context"],
-                max_length=384,
-                padding="max_length",
-                truncation="only_second",
-                return_offsets_mapping=True,
-            )
-
-            answers = examples["answers"]
-            offset_mapping: list[list[tuple[int, int]]] = inputs.pop("offset_mapping")
-
-            start_positions: list[int] = []
-            end_positions: list[int] = []
-            for i, (offset, answer) in enumerate(zip(offset_mapping, answers)):
-                start_chr = answer["answer_start"][0]
-                end_chr = start_chr + len(answer["text"][0])
-                start_pos, end_pos = self._get_squad_offset_span(
-                    offset=offset,
-                    seq_ids=inputs.sequence_ids(i),
-                    start_chr=start_chr,
-                    end_chr=end_chr,
-                )
-
-                start_positions.append(start_pos)
-                end_positions.append(end_pos)
-
-            inputs["start_positions"] = start_positions
-            inputs["end_positions"] = end_positions
-
-            return inputs
-
-        self.data = self.data.map(
-            _preprocess,
+        train = self.data["train"].map(
+            self._preprocess_train_batch,
             batched=True,
             remove_columns=self.data["train"].column_names,
         )
 
+        val = self.data["validation"].map(
+            self._preprocess_val_batch,
+            batched=True,
+            remove_columns=self.data["validation"].column_names,
+        )
+
+        return DatasetDict({"train": train, "validation": val})
+
+    def _preprocess_train_batch(self, examples: SQuADBatch):
+        questions = [q.strip() for q in examples["question"]]
+        inputs = self.tokenizer(
+            questions,
+            examples["context"],
+            stride=self._stride,
+            padding="max_length",
+            max_length=self._max_len,
+            truncation="only_second",
+            return_offsets_mapping=True,
+            return_overflowing_tokens=True,
+        )
+
+        answers = examples["answers"]
+        offset_mapping: list[list[tuple[int, int]]] = inputs.pop("offset_mapping")
+        sample_map: list[int] = inputs.pop("overflow_to_sample_mapping")
+
+        start_positions: list[int] = []
+        end_positions: list[int] = []
+        for i, (offset, sample_idx) in enumerate(zip(offset_mapping, sample_map)):
+            answer = answers[sample_idx]
+            start_chr = answer["answer_start"][0]
+            end_chr = answer["answer_start"][0] + len(answer["text"][0])
+            start_pos, end_pos = self._find_label_span(
+                offset=offset,
+                seq_ids=inputs.sequence_ids(i),
+                answer_span=(start_chr, end_chr),
+            )
+
+            start_positions.append(start_pos)
+            end_positions.append(end_pos)
+
+        inputs["start_positions"] = start_positions
+        inputs["end_positions"] = end_positions
+
+        return inputs
+
+    def _preprocess_val_batch(self, examples: SQuADBatch):
+        questions = [q.strip() for q in examples["question"]]
+        inputs = self.tokenizer(
+            questions,
+            examples["context"],
+            stride=self._stride,
+            padding="max_length",
+            max_length=self._max_len,
+            truncation="only_second",
+            return_offsets_mapping=True,
+            return_overflowing_tokens=True,
+        )
+
+        sample_map: list[int] = inputs.pop("overflow_to_sample_mapping")
+        example_ids: list[int] = []
+        for i in range(len(inputs.data["input_ids"])):
+            sample_idx = sample_map[i]
+            example_ids.append(examples["id"][sample_idx])
+
+            sequence_ids = inputs.sequence_ids(i)
+            inputs.data["offset_mapping"][i] = [
+                o if sequence_ids[k] == 1 else None
+                for k, o in enumerate(inputs.data["offset_mapping"][i])
+            ]
+
+        inputs["example_id"] = example_ids
+
+        return inputs
+
     @staticmethod
-    def _get_squad_offset_span(
+    def _find_label_span(
         offset: list[tuple[int, int]],
         seq_ids: list[int | None],
-        start_chr: int,
-        end_chr: int,
+        answer_span: tuple[int, int],
     ) -> tuple[int, int]:
+        start_chr, end_chr = answer_span
+
         idx = 0
         while seq_ids[idx] != 1:
             idx += 1
