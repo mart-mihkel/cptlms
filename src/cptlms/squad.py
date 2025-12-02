57f7a98a9310bebed4f69e97ea8ca2062949dac1
mart-mihkel <mart.mihkel.aun@gmail.com>
2025-12-02 15:33:52 +0200

    qa dataset and trainer

---
+++ src/cptlms/squad.py
@@ -0,0 +1,262 @@
+import logging
+from collections import defaultdict
+from typing import TypedDict, cast
+
+import evaluate
+import numpy as np
+from datasets.arrow_dataset import Dataset
+from datasets.dataset_dict import DatasetDict
+from datasets.load import load_dataset
+from numpy.typing import NDArray
+from tqdm.auto import tqdm
+from transformers import PreTrainedTokenizerFast
+
+logger = logging.getLogger(__name__)
+
+type OffsetMapping = list[list[tuple[int, int]]]
+
+
+class SQuADMetrics(TypedDict):
+    exact_match: float
+    f1: float
+
+
+class SQuADBatchAnswer(TypedDict):
+    answer_start: tuple[int]
+    text: tuple[str]
+
+
+class SQuADBatch(TypedDict):
+    id: list[int]
+    question: list[str]
+    context: list[str]
+    answers: list[SQuADBatchAnswer]
+
+
+class SQuAD:
+    def __init__(
+        self,
+        tokenizer: PreTrainedTokenizerFast,
+        max_len=384,
+        stride=128,
+    ):
+        logger.info("init SQuAD")
+
+        data = load_dataset("squad")
+        assert isinstance(data, DatasetDict)
+
+        self.metric = evaluate.loading.load("squad")
+
+        self.train = data["train"]
+        self.val = data["validation"]
+
+        self.max_len = max_len
+        self.stride = stride
+        self.tokenizer = tokenizer
+
+        self.train_tok, self.val_tok = self._tokenize()
+
+    def _tokenize(self) -> tuple[Dataset, Dataset]:
+        logger.info("tokenize SQuAD")
+
+        train = self.train.map(
+            self._preprocess_train_batch,
+            batched=True,
+            remove_columns=self.train.column_names,
+        )
+
+        val = self.val.map(
+            self._preprocess_val_batch,
+            batched=True,
+            remove_columns=self.val.column_names,
+        )
+
+        return train, val
+
+    def _preprocess_train_batch(self, examples: SQuADBatch):
+        questions = [q.strip() for q in examples["question"]]
+        inputs = self.tokenizer(
+            questions,
+            examples["context"],
+            padding="max_length",
+            truncation="only_second",
+            return_offsets_mapping=True,
+            return_overflowing_tokens=True,
+            stride=self.stride,
+            max_length=self.max_len,
+        )
+
+        answers = examples["answers"]
+        offset_mapping: OffsetMapping = inputs.pop("offset_mapping")
+        sample_map: list[int] = inputs.pop("overflow_to_sample_mapping")
+
+        start_positions: list[int] = []
+        end_positions: list[int] = []
+        for i, (offset, sample_idx) in enumerate(zip(offset_mapping, sample_map)):
+            answer = answers[sample_idx]
+            start_chr = answer["answer_start"][0]
+            end_chr = answer["answer_start"][0] + len(answer["text"][0])
+            start_pos, end_pos = self._find_label_span(
+                offset=offset,
+                seq_ids=inputs.sequence_ids(i),
+                answer_span=(start_chr, end_chr),
+            )
+
+            start_positions.append(start_pos)
+            end_positions.append(end_pos)
+
+        inputs["start_positions"] = start_positions
+        inputs["end_positions"] = end_positions
+
+        return inputs
+
+    def _preprocess_val_batch(self, examples: SQuADBatch):
+        questions = [q.strip() for q in examples["question"]]
+        inputs = self.tokenizer(
+            questions,
+            examples["context"],
+            padding="max_length",
+            truncation="only_second",
+            return_offsets_mapping=True,
+            return_overflowing_tokens=True,
+            stride=self.stride,
+            max_length=self.max_len,
+        )
+
+        sample_map: list[int] = inputs.pop("overflow_to_sample_mapping")
+        example_ids: list[int] = []
+        for i in range(len(inputs.data["input_ids"])):
+            sample_idx = sample_map[i]
+            example_ids.append(examples["id"][sample_idx])
+
+            sequence_ids = inputs.sequence_ids(i)
+            inputs.data["offset_mapping"][i] = [
+                o if sequence_ids[k] == 1 else None
+                for k, o in enumerate(inputs.data["offset_mapping"][i])
+            ]
+
+        inputs["example_id"] = example_ids
+
+        return inputs
+
+    @staticmethod
+    def _find_label_span(
+        offset: list[tuple[int, int]],
+        seq_ids: list[int | None],
+        answer_span: tuple[int, int],
+    ) -> tuple[int, int]:
+        start_chr, end_chr = answer_span
+
+        idx = 0
+        while seq_ids[idx] != 1:
+            idx += 1
+
+        ctx_start = idx
+
+        while seq_ids[idx] == 1:
+            idx += 1
+
+        ctx_end = idx - 1
+
+        if offset[ctx_start][0] > end_chr or offset[ctx_end][1] < start_chr:
+            return 0, 0
+
+        idx = ctx_start
+        while idx <= ctx_end and offset[idx][0] <= start_chr:
+            idx += 1
+
+        start_pos = idx - 1
+
+        idx = ctx_end
+        while idx >= ctx_start and offset[idx][1] >= end_chr:
+            idx -= 1
+
+        end_pos = idx + 1
+
+        return start_pos, end_pos
+
+    def compute_metrics(
+        self,
+        start_logits: NDArray[np.float64],
+        end_logits: NDArray[np.float64],
+    ) -> SQuADMetrics:
+        predicted_answers = self._postprocess_predictions(
+            start_logits=start_logits,
+            end_logits=end_logits,
+        )
+
+        theoretical_answers = [
+            {"id": ex["id"], "answers": ex["answers"]} for ex in self.val
+        ]
+
+        metrics = self.metric.compute(
+            predictions=predicted_answers,
+            references=theoretical_answers,
+        )
+
+        return cast(SQuADMetrics, metrics)
+
+    def _postprocess_predictions(
+        self,
+        start_logits: NDArray[np.float64],
+        end_logits: NDArray[np.float64],
+    ) -> list[dict[str, str | int]]:
+        example_to_features: dict[int, list[int]] = defaultdict(list)
+        for idx, feature in enumerate(self.val_tok):
+            example_to_features[feature["example_id"]].append(idx)
+
+        predicted_answers = []
+        for example in tqdm(self.val, desc="postprocess"):
+            example_id = example["id"]
+            answers = self._extract_answers(
+                start_logits=start_logits,
+                end_logits=end_logits,
+                context=example["context"],
+                example_features=example_to_features[example_id],
+                offset_mapping=self.val_tok["offset_mapping"],
+            )
+
+            if len(answers) > 0:
+                best_answer = max(answers, key=lambda x: x["logit_score"])
+                predicted_answers.append(
+                    {"id": example_id, "prediction_text": best_answer["text"]}
+                )
+            else:
+                predicted_answers.append({"id": example_id, "prediction_text": ""})
+
+        return predicted_answers
+
+    @staticmethod
+    def _extract_answers(
+        start_logits: NDArray[np.float64],
+        end_logits: NDArray[np.float64],
+        context: list[str],
+        example_features: list[int],
+        offset_mapping: OffsetMapping,
+        n_best: int = 20,
+        max_answer_len: int = 30,
+    ) -> list[dict[str, str | int]]:
+        answers = []
+        for f_idx in example_features:
+            start_logit = start_logits[f_idx]
+            end_logit = end_logits[f_idx]
+            offsets = offset_mapping[f_idx]
+
+            start_indexes = np.argsort(start_logit)[-1 : -n_best - 1 : -1].tolist()
+            end_indexes = np.argsort(end_logit)[-1 : -n_best - 1 : -1].tolist()
+            for start_index in start_indexes:
+                for end_index in end_indexes:
+                    if offsets[start_index] is None or offsets[end_index] is None:
+                        continue
+
+                    if (
+                        end_index < start_index
+                        or end_index - start_index + 1 > max_answer_len
+                    ):
+                        continue
+
+                    text = context[offsets[start_index][0] : offsets[end_index][1]]
+                    score = start_logit[start_index] + end_logit[end_index]
+                    answers.append({"text": text, "logit_score": score})
+
+        return answers
---
+++ src/cptlms/trainer.py
@@ -0,0 +1,150 @@
+import logging
+import os
+from pathlib import Path
+from typing import Callable, TypedDict
+
+import numpy as np
+import torch
+from accelerate import Accelerator
+from torch.nn.modules.module import Module
+from torch.optim import AdamW
+from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+from transformers import get_scheduler
+from transformers.modeling_outputs import QuestionAnsweringModelOutput
+
+from cptlms.squad import SQuAD, SQuADMetrics
+
+logger = logging.getLogger(__name__)
+
+
+class Telemetry(TypedDict):
+    metrics: list[SQuADMetrics]
+    train_loss: list[float]
+
+
+class Trainer:
+    def __init__(
+        self,
+        model: Module,
+        epochs: int,
+        qa_dataset: SQuAD,
+        collate_fn: Callable,
+        batch_size: int = 16,
+        out_dir: Path = Path("out/qa"),
+    ) -> None:
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.dataset = qa_dataset
+        self.out_dir = out_dir
+        self.telemetry: Telemetry = {"metrics": [], "train_loss": []}
+
+        model = model.to(self.device)
+
+        train_loader = DataLoader(
+            qa_dataset.train_tok.with_format("torch", device=self.device),
+            batch_size=batch_size,
+            shuffle=True,
+            collate_fn=collate_fn,
+        )
+
+        val_loader = DataLoader(
+            qa_dataset.val_tok.with_format("torch", device=self.device),
+            batch_size=batch_size,
+            collate_fn=collate_fn,
+        )
+
+        optimizer = AdamW(
+            model.parameters(),
+            lr=2e-5,
+            weight_decay=0.01,
+        )
+
+        self.accelerator = Accelerator(mixed_precision="fp16")
+        self.model, self.optimizer, self.train_loader, self.val_loader = (
+            self.accelerator.prepare(
+                model,
+                optimizer,
+                train_loader,
+                val_loader,
+            )
+        )
+
+        self.model: Module
+        self.optimizer: AdamW
+        self.train_loader: DataLoader
+        self.val_loader: DataLoader
+
+        self.scheduler: LambdaLR = get_scheduler(
+            "linear",
+            num_warmup_steps=0,
+            optimizer=self.optimizer,
+            num_training_steps=len(self.train_loader) * self.epochs,
+        )
+
+    def train(self):
+        self.model.train()
+        for epoch in range(1, self.epochs + 1):
+            logger.info("epoch %d/%d", epoch, self.epochs)
+            self._epoch()
+            self._save_checkpoint(epoch=epoch)
+
+    def _epoch(self):
+        train_loss = 0
+
+        for batch in tqdm(self.train_loader, desc="train"):
+            outputs = self.model(**batch)
+            loss = outputs.loss
+            self.accelerator.backward(loss)
+
+            self.optimizer.step()
+            self.scheduler.step()
+            self.optimizer.zero_grad()
+
+            train_loss += loss.item()
+
+        avg_train_loss = train_loss / len(self.train_loader)
+        logger.info("training loss: %.4f", avg_train_loss)
+
+        metrics = self._eval()
+        logger.info("exact match: %.4f", metrics["exact_match"])
+        logger.info("f1: %.4f", metrics["f1"])
+
+        self.telemetry["train_loss"].append(avg_train_loss)
+        self.telemetry["metrics"].append(metrics)
+
+    def _eval(self) -> SQuADMetrics:
+        self.model.eval()
+
+        start_logits = []
+        end_logits = []
+
+        for batch in tqdm(self.val_loader, desc="eval"):
+            with torch.no_grad():
+                outputs: QuestionAnsweringModelOutput = self.model(**batch)
+
+            out_starts = self.accelerator.gather(outputs.start_logits)
+            out_end = self.accelerator.gather(outputs.end_logits)
+
+            assert isinstance(out_starts, torch.Tensor)
+            assert isinstance(out_end, torch.Tensor)
+
+            start_logits.append(out_starts.cpu().numpy())
+            end_logits.append(out_end.cpu().numpy())
+
+        start_logits = np.concatenate(start_logits)
+        end_logits = np.concatenate(end_logits)
+
+        n = len(self.val_loader) * self.batch_size
+        start_logits = start_logits[:n]
+        end_logits = end_logits[:n]
+
+        return self.dataset.compute_metrics(start_logits, end_logits)
+
+    def _save_checkpoint(self, epoch: int):
+        os.makedirs(self.out_dir, exist_ok=True)
+        save_path = self.out_dir / f"chkpt-{epoch}.pth"
+        logger.info("save model state dict to %s", save_path)
+        torch.save(self.model.state_dict(), save_path)
